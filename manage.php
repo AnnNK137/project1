e6ec5af8241d13d74326acd85f6fbdbb830a41cc
Kaimestry <khanhan137@outlook.com>
2025-11-08 17:22:11 +0700

    listed requirement for manage.php

---
+++ manage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HR MANAGEMENT</title>
+</head>
+<body>
+    <!-- • List all EOIs. -->
+     
+    <!-- • List all EOIs for a particular position (given a job reference number). -->
+     
+    <!-- • List all EOIs for a particular applicant given their first name, last name or both. -->
+     
+    <!-- • Delete all EOIs with a specified job reference number -->
+     
+    <!-- • Change the Status of an EOI. -->
+     
+</body>
+</html>
